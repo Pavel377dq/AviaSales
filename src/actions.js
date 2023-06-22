4df6f83d42041d813d22acfa60acc7248f4a3c80
Pavel Poryvai <42907367+Pavel377dq@users.noreply.github.com>
2023-06-22 16:19:48 +0300

    delete extra actions

--- src/actions.js
+++ src/actions.js
@@ -2,26 +2,6 @@
 
 import api from './Api/Api';
 
-export const doAllOn = () => ({ type: 'markAllOn' });
-
-export const doAllOff = () => ({ type: 'markAllOff' });
-
-export const doNoTransferOn = () => ({ type: 'markNoTransferOn' });
-
-export const doNoTransferOff = () => ({ type: 'markNoTransferOff' });
-
-export const doOneTransferOn = () => ({ type: 'markOneTransferOn' });
-
-export const doOneTransferOff = () => ({ type: 'markOneTransferOff' });
-
-export const doTransferTwoOn = () => ({ type: 'markTransferTwoOn' });
-
-export const doTransferTwoOff = () => ({ type: 'markTransferTwoOff' });
-
-export const doTransferThreeOff = () => ({ type: 'markTransferThreeOff' });
-
-export const doTransferThreeOn = () => ({ type: 'markTransferThreeOn' });
-
 export const initData = (data, isStop) => ({ type: 'initData', data, stop: isStop });
 
 export const changeSortTag = (name) => ({ type: 'CHANGE_SORT_TAG', name });
